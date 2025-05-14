aa1705f51e93e4cf9333ab8c4c8d6ae4f496841d
Vinícius Brunheroto <vinibrunheroto12@gmail.com>
2025-05-14 18:19:09 -0300

    fix errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,10 @@
-import Link from 'next/link'
-import { FaGithub } from 'react-icons/fa6'
+import { Suspense } from 'react'
+import PortalClient from '@/app/modules/portal/components/portal-client'
 
-export default function Home() {
+export default function PortalPage() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      <h1 className="text-4xl font-bold">Homepage do Site</h1>
-      <hr />
-      <nav className="mt-6">
-        <Link href="/portal" className="text-blue-500">
-          {' '}
-          Acesse o Portal{' '}
-        </Link>
-        ou{' '}
-        <Link href="/portal/cadastro" className="text-blue-500">
-          {' '}
-          Crie uma conta
-        </Link>
-      </nav>
-
-      <Link
-        href="https://github.com/viniciusbrunheroto/sistema-auth"
-        target="_blank"
-        className="hover:text-blue-500 transition-all duration-300 py-4 inline-block"
-      >
-        <span className="flex gap-2 items-center underline">
-          {' '}
-          Acessar esse projeto no <FaGithub size={30} />
-        </span>
-      </Link>
-    </main>
+    <Suspense fallback={null}>
+      <PortalClient />
+    </Suspense>
   )
 }
