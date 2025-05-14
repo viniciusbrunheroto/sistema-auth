a5343d93952ec65dd8adbb706a9650d0c5673e06
Vinícius Brunheroto <vinibrunheroto12@gmail.com>
2025-05-14 18:21:22 -0300

    fix errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,33 @@
-import { Suspense } from 'react'
-import PortalClient from '@/app/modules/portal/components/portal-client'
+import Link from 'next/link'
+import { FaGithub } from 'react-icons/fa6'
 
-export default function PortalPage() {
+export default function Home() {
   return (
-    <Suspense fallback={null}>
-      <PortalClient />
-    </Suspense>
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1 className="text-4xl font-bold">Homepage do Site</h1>
+      <hr />
+      <nav className="mt-6">
+        <Link href="/portal" className="text-blue-500">
+          {' '}
+          Acesse o Portal{' '}
+        </Link>
+        ou{' '}
+        <Link href="/portal/cadastro" className="text-blue-500">
+          {' '}
+          Crie uma conta
+        </Link>
+      </nav>
+
+      <Link
+        href="https://github.com/viniciusbrunheroto/sistema-auth"
+        target="_blank"
+        className="hover:text-blue-500 transition-all duration-300 py-4 inline-block"
+      >
+        <span className="flex gap-2 items-center underline">
+          {' '}
+          Acessar esse projeto no <FaGithub size={30} />
+        </span>
+      </Link>
+    </main>
   )
 }
--- src/app/portal/page.tsx
+++ src/app/portal/page.tsx
@@ -1,30 +1,10 @@
-'use client'
-
-import Link from 'next/link'
-import UsersList from '../modules/users/components/users-list'
-import { useEffect } from 'react'
-import { toast } from 'react-toastify'
-import { useSearchParams } from 'next/navigation'
+import { Suspense } from 'react'
+import PortalClient from '@/app/modules/portal/components/portal-client'
 
 export default function PortalPage() {
-  const searchParams = useSearchParams()
-
-  useEffect(() => {
-    const success = searchParams.get('success')
-
-    if (success === 'login')
-      toast.success('Login com sucesso!', {
-        position: 'bottom-right',
-      })
-  }, [searchParams])
-
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      <h1 className="text-4xl font-bold">Página do Portal</h1>
-
-      <UsersList />
-
-      <Link href="/api/logout">Logout</Link>
-    </main>
+    <Suspense fallback={null}>
+      <PortalClient />
+    </Suspense>
   )
 }
